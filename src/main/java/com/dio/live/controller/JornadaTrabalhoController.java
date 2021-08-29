3d4bd117f4882077208000c2e9b325854e8d890c
Lucas Figueiredo <lukas.liberato14@gmail.com>
2021-08-29 15:25:54 -0300

    Aula 11 - DIO

--- src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -1,9 +1,39 @@
 package com.dio.live.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.repository.JornadaRepository;
+import com.dio.live.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
-@RequestMapping["/jornada"] // encontrar endpoint
+@RequestMapping("/jornada") // encontrar endpoint
 public class JornadaTrabalhoController {
+
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+//    public JornadaTrabalho getJornadaByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+//        return jornadaService.getById(idJornada).orElseThrow(() ->  new Exception("jornada não encontrada"));
+//    }
+
+    public ResponseEntity<JornadaTrabalho> getJornadaByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() ->  new Exception("jornada não encontrada")));
+    }
+
 }
+
--- src/main/java/com/dio/live/service/JornadaService.java
+++ src/main/java/com/dio/live/service/JornadaService.java
@@ -1,8 +1,31 @@
 package com.dio.live.service;
 
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JornadaService {
 
+    JornadaRepository jornadaRepository;
+
+    @Autowired // não precisa criar or construtor
+    public JornadaService(JornadaRepository jornadaRepository){
+        this.jornadaRepository = jornadaRepository;
+    }
+
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> findAll(){
+        return jornadaRepository.findAll();
+    }
+    public Optional<JornadaTrabalho> getById(Long idJornada) {
+        return jornadaRepository.findById(idJornada);
+    }
 }
